17802990fb2a6ab3b5a5bb1d53a809b2847757f9
ismet kizgin <info@ismetkizgin.com>
2020-06-25 01:54:37 +0300

    feat: added verify token middleware

---
+++ src/middleware/verifyToken.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+export const verifyToken = (req, res, next) => {
+    const token = req.headers['token'] || req.body.token || req.query.token
+    if (token) {
+        jwt.verify(token, req.app.get('api_key'), (err, decoded) => {
+
+            if (err) {
+                res.json({
+                    status: false,
+                    message: 'Failed to authentication token.'
+                })
+            } else {
+                req.decode = decoded,
+                    next();
+            }
+        });
+
+    } else {
+        res.json({
+            status: false,
+            message: 'No token provided.'
+        })
+    }
+};
