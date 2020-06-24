dac6013d260f414e1c913e183d1d328f3235ed6b
ismet kizgin <info@ismetkizgin.com>
2020-06-24 17:46:58 +0300

    feat: added main file

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import path from 'path'
+import bodyParser from 'body-parser'
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('api_key', process.env.API_KEY || 'secret');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    res.send("404 NOT FOUND");
+})
+
+app.listen(PORT, () => {
+    console.log("Ready on http://localhost:" + PORT)
+});
