6bf45e5ff20a515a91bdcbad73dcd9bced96deda
ismet kizgin <info@ismetkizgin.com>
2020-06-26 19:23:34 +0300

    feat: added config file for routers

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
-import path from 'path'
 import bodyParser from 'body-parser'
+import routers from './serve'
 
-const routers = require('./routers');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
@@ -14,16 +13,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('api_key', process.env.API_KEY || 'secret');
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routers.authRouter);
-app.use(routers.slideRouter);
-app.use(routers.pageRouter);
-app.use(routers.InstantEarthquakesRouter);
-
-app.use((req, res, next) => {
-    res.send("404 NOT FOUND");
-})
+app.use(routers);
 
 app.listen(PORT, () => {
     console.log("Ready on http://localhost:" + PORT)
