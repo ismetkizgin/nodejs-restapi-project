23ee505dabf9d848d16dc2ec0f5efbbad012853d
ismet kizgin <info@ismetkizgin.com>
2020-07-01 01:53:13 +0300

    fix: http status code and error message revision for auth validators

--- src/middleware/validators/authValidator.js
+++ src/middleware/validators/authValidator.js
@@ -1,11 +1,12 @@
 import { isAlpha, isEmail, isEmpty, isInt, isMobilePhone } from '../functions/validatorFunctions'
+import { validateMessage } from '../../fixtures/messageStatus.json'
 
 module.exports = {
     login: (req, res, next) => {
         if (isInt(req.body.UserIdentityNo) && !isEmpty(req.body.UserPassword))
             next();
         else
-            res.send({ status: false, message: 'Must have correct data entry' });
+            res.send({ status: validateMessage.status, message: validateMessage.message });
     },
 
     all: (req, res, next) => {
@@ -21,14 +22,13 @@ module.exports = {
         if (state)
             next();
         else
-            res.send({ status: false, message: 'Must have correct data entry' });
-
+            res.send({ status: validateMessage.status, message: validateMessage.message });
     },
 
     deleteMyAccount: (req, res, next) => {
         if (isInt(req.body.UserIdentityNo))
             next();
         else
-            res.send({ status: false, message: 'Must have correct data entry' });
+            res.send({ status: validateMessage.status, message: validateMessage.message });
     }
 }
