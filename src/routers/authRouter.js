9cace45d933c9e42008a92bee1814e8112593f55
ismet kizgin <info@ismetkizgin.com>
2020-09-12 15:35:57 +0300

    fix: fix for user transactions and routers

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -10,13 +10,9 @@ const userTransactions = dbFactory('userTransactions');
 router.post('/login', authValidator.login, async (req, res) => {
 	try {
 		const result = await userTransactions.login(req.body);
-		if (result.status != 404 && result.status != 500) {
-			const payload = { UserIdentityNo: result.UserIdentityNo, UserStatus: result.UserStatusName };
-			const token = jwt.sign(payload, req.app.get('api_key'), { expiresIn: 720 });
-			res.json({ result, token });
-		} else {
-			res.status(result.status).json({ message: result.message });
-		}
+		const payload = { UserIdentityNo: result.UserIdentityNo, UserStatus: result.UserStatusName };
+		const token = jwt.sign(payload, req.app.get('api_key'), { expiresIn: 720 });
+		res.json({ userInformation: result, token });
 	} catch (err) {
 		res.status(err.status).json({ message: err.message });
 	}
@@ -24,11 +20,8 @@ router.post('/login', authValidator.login, async (req, res) => {
 
 router.post('/sign-up', authValidator.all, async (req, res) => {
 	try {
-		const findUserIdentityNo = await userTransactions.findUserIdentityNo(req.body.UserIdentityNo);
-		if (findUserIdentityNo.status == 404) {
-			const result = await userTransactions.signup(req.body);
-			res.status(result.status).json({message: result.message});
-		} else res.status(userMessage.SignUp_Router_Bad_Request.status).json({ message: userMessage.SignUp_Router_Bad_Request.message });
+		const result = await userTransactions.signup(req.body);
+		res.json({ message: result.message });
 	} catch (err) {
 		res.status(err.status).json({ message: err.message });
 	}
@@ -38,9 +31,9 @@ router.delete('/delete-my-account', verifyToken, async (req, res) => {
 	try {
 		if (req.decode.UserIdentityNo == req.body.UserIdentityNo) {
 			const result = await userTransactions.delete(req.body.UserIdentityNo);
-			res.status(result.status).json({message: result.message});
+			res.status(result.status).json({ message: result.message });
 		} else {
-			res.status(userMessage.DeleteMyAccount_Router_Proxy_Authentication_Required.status).json({ message: userMessage.DeleteMyAccount_Router_Proxy_Authentication_Required.message});
+			res.status(userMessage.delete.Proxy_Authentication_Required.status).json({ message: userMessage.delete.Proxy_Authentication_Required.message });
 		}
 	} catch (err) {
 		res.status(err.status).json({ message: err.message });
