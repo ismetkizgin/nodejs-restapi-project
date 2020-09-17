22308e20cb206be63fbdf57fd66e56ea40efbdec
AyberkCakar <ayberkcakar35@gmail.com>
2020-09-18 01:12:01 +0300

    refactor: validator structure has been changed.

--- src/middleware/functions/validatorFunctions.js
+++
@@ -1,7 +0,0 @@
-let validator = require('validator');
-
-export function isInt(value) { return value == null ? false : validator.isInt(value) };
-export function isEmpty(value) { return value == null ? true : validator.isEmpty(value) };
-export function isAlpha(value) { return value == null ? false : validator.isAlpha(value, ['tr-TR']) };
-export function isMobilePhone(value) { return value == null ? false : validator.isMobilePhone(value, ['tr-TR']) };
-export function isEmail(value) { return value == null ? false : validator.isEmail(value) };
--- src/middleware/validators/institutionValidator.js
+++ src/middleware/validators/institutionValidator.js
@@ -1,31 +1,43 @@
-import { isEmpty } from '../functions/validatorFunctions'
 import { validateMessage } from '../../fixtures/messageStatus.json'
+import joi from "joi/lib/index";
 
 module.exports = {
     add: async (req, res, next) => {
-        const body = req.body;
-        const state = !isEmpty(body.InstitutionName)
-            && !isEmpty(body.InstitutionNo) && !isEmpty(body.InstitutionEmail);
-        if (state)
+        try {
+            const addSchema = joi.object({
+                InstitutionName:joi.string().min(3).pattern(new RegExp('^[A-Za-zÇçÖöŞşÜüĞğİı]+$')).required(),
+                InstitutionPhone:joi.string().min(11).max(11).pattern(new RegExp('^[0-9]+$')).required(),
+                InstitutionEmail:joi.string().email().required(),
+            });
+            await addSchema.validateAsync(req.body);
             next();
-        else
+        } catch (error) {
             res.status(validateMessage.status).send({ message: validateMessage.message });
+        }
     },
     update: async (req, res, next) => {
-        const body = req.body;
-        const state = !isEmpty(body.InstitutionID) && !isEmpty(body.InstitutionName)
-            && !isEmpty(body.InstitutionNo) && !isEmpty(body.InstitutionEmail);
-        if (state)
+        try {
+            const updateSchema = joi.object({
+                InstitutionID:joi.number().min(1).max(99999999999).required(),
+                InstitutionName:joi.string().min(3).pattern(new RegExp('^[A-Za-zÇçÖöŞşÜüĞğİı]+$')).required(),
+                InstitutionPhone:joi.string().min(11).max(11).pattern(new RegExp('^[0-9]+$')).required(),
+                InstitutionEmail:joi.string().email().required(),
+            });
+            await updateSchema.validateAsync(req.body);
             next();
-        else
+        } catch (error) {
             res.status(validateMessage.status).send({ message: validateMessage.message });
+        }
     },
     delete: async (req, res, next) => {
-        const body = req.body;
-        const state = !isEmpty(body.InstitutionID);
-        if (state)
+        try {
+            const deleteSchema = joi.object({
+                InstitutionID:joi.number().min(1).max(99999999999).required(),
+            });
+            await deleteSchema.validateAsync(req.body);
             next();
-        else
+        } catch (error) {
             res.status(validateMessage.status).send({ message: validateMessage.message });
+        }
     }
 };
