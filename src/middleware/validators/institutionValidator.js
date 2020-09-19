668cc1f856681f2834ef6fba41e329da7140b25b
AyberkCakar <ayberkcakar35@gmail.com>
2020-09-19 13:29:39 +0300

    refactor: validator structure has been changed.

--- src/middleware/validators/institutionValidator.js
+++ src/middleware/validators/institutionValidator.js
@@ -4,12 +4,11 @@ import joi from "joi/lib/index";
 module.exports = {
     add: async (req, res, next) => {
         try {
-            const addSchema = joi.object({
+            await joi.object({
                 InstitutionName:joi.string().min(3).pattern(new RegExp('^[A-Za-zÇçÖöŞşÜüĞğİı]+$')).required(),
                 InstitutionPhone:joi.string().min(11).max(11).pattern(new RegExp('^[0-9]+$')).required(),
                 InstitutionEmail:joi.string().email().required(),
-            });
-            await addSchema.validateAsync(req.body);
+            }).validateAsync(req.body);
             next();
         } catch (error) {
             res.status(validateMessage.status).send({ message: validateMessage.message });
@@ -17,13 +16,12 @@ module.exports = {
     },
     update: async (req, res, next) => {
         try {
-            const updateSchema = joi.object({
+            await joi.object({
                 InstitutionID:joi.number().min(1).max(99999999999).required(),
                 InstitutionName:joi.string().min(3).pattern(new RegExp('^[A-Za-zÇçÖöŞşÜüĞğİı]+$')).required(),
                 InstitutionPhone:joi.string().min(11).max(11).pattern(new RegExp('^[0-9]+$')).required(),
                 InstitutionEmail:joi.string().email().required(),
-            });
-            await updateSchema.validateAsync(req.body);
+            }).validateAsync(req.body);
             next();
         } catch (error) {
             res.status(validateMessage.status).send({ message: validateMessage.message });
@@ -31,10 +29,9 @@ module.exports = {
     },
     delete: async (req, res, next) => {
         try {
-            const deleteSchema = joi.object({
+            await joi.object({
                 InstitutionID:joi.number().min(1).max(99999999999).required(),
-            });
-            await deleteSchema.validateAsync(req.body);
+            }).validateAsync(req.body);
             next();
         } catch (error) {
             res.status(validateMessage.status).send({ message: validateMessage.message });
