5185f55f9f3a1a1fa41c71c32ce619ece93d78bf
ismet kizgin <info@ismetkizgin.com>
2020-10-18 23:08:13 +0300

    refactor: router layer revised

---
+++ routers/authRouter.js
@@ -0,0 +1,60 @@
+const router = require('express')();
+const jwt = require('jsonwebtoken');
+const TransactionsFactory = require('../database/transactionFactory');
+const userTransactions = TransactionsFactory.creating('userTransactions');
+const institutionUserTransactions = TransactionsFactory.creating('institutionUserTransactions');
+const { validator, verifyToken } = require('../middleware');
+const authValidator = validator.authValidator;
+const tokenControl = verifyToken.tokenControl;
+const HttpStatusCode = require('http-status-codes');
+
+router.post('/login/:loginType', authValidator.login, async (req, res) => {
+	try {
+		let result, payload;
+		switch (req.params.loginType) {
+			case 'user':
+				result = await userTransactions.login(req.body);
+				payload = { UserIdentityNo: result.UserIdentityNo, UserStatus: result.UserStatusName };
+				break;
+			case 'institution':
+				result = await institutionUserTransactions.login(req.body);
+				payload = { InstitutionUserID: result.institutionUserID };
+				break;
+			default:
+				res.status(HttpStatusCode.BAD_REQUEST).json('The entry type is not correct.');
+				return;
+		}
+		const token = jwt.sign(payload, req.app.get('api_key'), { expiresIn: 720 });
+		res.json({ userInformation: result, token });
+	} catch (err) {
+		res.status(err.status).json(err.message);
+	}
+});
+
+router.post('/sign-up', authValidator.signUp, async (req, res) => {
+	try {
+		const result = await userTransactions.signup(req.body);
+		res.json(result);
+	} catch (err) {
+		res.status(err.status).json(err.message);
+	}
+});
+
+router.delete('/delete-my-account', tokenControl, async (req, res) => {
+	try {
+		if (req.decode.UserIdentityNo == req.body.UserIdentityNo) {
+			const result = await userTransactions.delete(req.body.UserIdentityNo);
+			res.status(result.status).json({ message: result.message });
+		} else {
+			res.status(HttpStatusCode.PROXY_AUTHENTICATION_REQUIRED).json('You are not authorized to do this !');
+		}
+	} catch (err) {
+		res.status(err.status).json(err.message);
+	}
+});
+
+router.get('/token-decode', tokenControl, async (req, res) => {
+	res.json(req.decode);
+});
+
+module.exports = router;
---
+++ routers/institutionRouter.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import { verifyToken, institutionValidator } from '../middleware'
+import dbFactory from '../database'
+
+const router = express();
+const institutionTransactions = dbFactory('institutionTransactions');
+
+router.get('/institution', async (req, res) => {
+    try {
+        const response = await institutionTransactions.list();
+        res.json(response);
+    } catch (error) {
+        res.status(error.status).json({ message: error.message });
+    }
+});
+
+router.post('/institution', verifyToken,institutionValidator.add, async (req, res) => {
+    try {
+        const response = await institutionTransactions.insert(req.body);
+        res.json({message:response.message});
+    } catch (error) {
+        res.status(error.status).json({ message: error.message });
+    }
+});
+router.put('/institution', verifyToken,institutionValidator.update, async (req, res) => {
+    try {
+        const response = await institutionTransactions.update(req.body);
+        res.json({message:response.message});
+    } catch (error) {
+        res.status(error.status).json({ message: error.message });
+    }
+});
+router.delete('/institution', verifyToken, institutionValidator.delete, async (req, res) => {
+    try {
+        const response = await institutionTransactions.delete(req.body.InstitutionID);
+        res.json({message:response.message});
+    } catch (error) {
+        res.status(error.status).json({ message: error.message });
+    }
+});
+
+module.exports = router;
